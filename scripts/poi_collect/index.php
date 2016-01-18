378b80b5cd1c601d48dfc325c1ceb47cf5e43d9e
Veit UR <veit.stephan@ukr.de>
2016-01-18 12:39:37 +0100

    Setup Final
    Also SecVars integreated

--- scripts/poi_collect/index.php
+++ scripts/poi_collect/index.php
@@ -1,3 +1,4 @@
+<?php include '../../sec/givexml.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,6 +20,10 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+<script>
+    var secData = <?php echo json_encode($secKeys); ?>;
+		console.log(secData);
+</script>
 </head>
 
 <body role="document">
