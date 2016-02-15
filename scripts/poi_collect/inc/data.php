1bf1546cac0fc32f3536e4fefa7271f884822c37
Veit UR <veit.stephan@ukr.de>
2016-02-15 18:52:00 +0100

    Integrate Radar Search
    Now Radar Search is possible

--- scripts/poi_collect/inc/data.php
+++ scripts/poi_collect/inc/data.php
@@ -123,10 +123,9 @@
     }
     if($info->next_page_token) {
       callAddToken($info->next_page_token);
-      echo "<hr />";
-      echo "<hr />";
+    } else {
+      echo '<p><span style="color: orange">Just one result page</span>: No Next Page Token found at all!</p>';
     }
-
     echo "<h2>";
     echo "Anfrage Query " . $gSearchURL . "\n";
     echo "</h2>";
@@ -137,7 +136,109 @@
     echo "<h3>";
     echo "Gefilterte Einträge: <b>" . count($fpois) . "</b>\n";
     echo "</h3>";
-    
+
+    /*
+    formatResult($fpois);
+    */
+    if($doDebug) {
+      forDebug($fpois);
+    }
+  }
+
+  function radarOneType($jRobj, $typenr = 0, $doDebug = null) {
+    global $pois;
+    global $pids;
+    global $gSearchURL;
+    // String for request
+    $gSearchURL = "https://maps.googleapis.com/maps/api/place/radarsearch/json?location=" .
+    $jRobj->{'geometry'}->{'location'}->{'lat'} . "," . $jRobj->{'geometry'}->{'location'}->{'lng'} .
+    "&radius=1500" .
+    "&types=" . $jRobj->{'types'}[$typenr];
+    $gSearchURL = addAPIkey($gSearchURL);
+
+    $info = json_decode( file_get_contents($gSearchURL) );
+
+    foreach($info->results as $poi) {
+      $array =  (array) $poi;
+      array_push($pois, $poi);
+    }
+
+    // TODO: Check if Next Page is present at all!
+
+    echo "<h2>";
+    echo "Radar Anfrage für <span style=\"color: blue;\">" . $jRobj->{'types'}[$typenr] . "</span> Query " . $gSearchURL . "\n";
+    echo "</h2>";
+    echo "<h3>";
+    echo "Einträge: <b>" . count($pois) . "</b>\n";
+    echo "</h3>";
+    $fpois = array_filter($pois, "checkDuplicatePID");
+    echo "<h3>";
+    echo "Gefilterte Einträge: <b>" . count($fpois) . "</b>\n";
+    echo "</h3>";
+
+    /*
+    formatResult($fpois);
+    */
+    if($doDebug) {
+      forDebug($fpois);
+    }
+  }
+
+  function radarAllTypesConcat($jRobj, $doDebug = null) {
+    global $pois;
+    global $pids;
+    global $gSearchURL;
+
+    for ($i = 0; $i < count($jRobj->{'types'}); $i++) {
+      // String for request
+      $gSearchURL = "https://maps.googleapis.com/maps/api/place/radarsearch/json?location=" .
+      $jRobj->{'geometry'}->{'location'}->{'lat'} . "," . $jRobj->{'geometry'}->{'location'}->{'lng'} .
+      "&radius=1500" .
+      "&types=" . $jRobj->{'types'}[$i];
+      $gSearchURL = addAPIkey($gSearchURL);
+
+      $info = json_decode( file_get_contents($gSearchURL) );
+
+      foreach($info->results as $poi) {
+        $array =  (array) $poi;
+        array_push($pois, $poi);
+      }
+      echo "<h2>";
+      echo "Radar Anfrage für  1. Seite <span style=\"color: blue;\">" .  $jRobj->{'types'}[$i] . "</span> Query " . $gSearchURL . "\n";
+      echo "</h2>";
+      echo "<h3>";
+      echo "Einträge: <b>" . count($info->results) . "</b>\n";
+      echo "</h3>";
+      //forDebug($info);
+      echo "<p>#</p>";
+      echo "<p>#</p>";
+      echo "<p>#</p>";
+      echo "<p>#</p>";
+      echo "<p>#</p>";
+      echo "<p>#</p>";
+      echo "<p>#</p>";
+      echo "<p>#</p>";
+      echo "<p>#</p>";
+      echo "<p>#</p>";
+      echo "<p>#</p>";
+      echo "<p>#</p>";
+      echo "<p>#</p>";
+      echo "<p>#</p>";
+      echo "<p>#</p>";
+      echo "<p>#</p>";
+    }
+
+    echo "<h2>";
+    echo "Radar Anfrage für  1. Seite <span style=\"color: blue;\"> All Types</span> Query " . $gSearchURL . "\n";
+    echo "</h2>";
+    echo "<h3>";
+    echo "Einträge: <b>" . count($pois) . "</b>\n";
+    echo "</h3>";
+    $fpois = array_filter($pois, "checkDuplicatePID");
+    echo "<h3>";
+    echo "Gefilterte Einträge: <b>" . count($fpois) . "</b>\n";
+    echo "</h3>";
+
     /*
     formatResult($fpois);
     */
@@ -153,7 +254,9 @@
    *
    */
    // nearbyAllTypesFirstPage($jRobj, true);
-   nearbyAllPagesOneType($jRobj, 1);
+   // nearbyAllPagesOneType($jRobj, 0, true);
+   // radarOneType($jRobj, 0, true);
+   radarAllTypesConcat($jRobj);
 
   /*
    *
@@ -175,10 +278,6 @@
     $gAddSearchURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=" .
       $nextPageToken;
       $gAddSearchURL = addAPIkey($gAddSearchURL);
-    echo '<p><span style="color: orange">Page Token was True:</span> Next Token: ' . $nextPageToken . '</p>';
-    echo "<hr />";
-    print($gAddSearchURL);
-    echo "<hr />";
     // Wait 1 second, because next page token must be valid at google first!
     usleep(1000000);
     $addInfo = json_decode( file_get_contents($gAddSearchURL) );
@@ -188,7 +287,6 @@
         array_push($pois, $poi);
       }
     } else {
-      echo '<p><span style="color: orange">Next Page Token not valid yet, wait …</p>';
       usleep(500000);
       $addInfo = json_decode( file_get_contents($gAddSearchURL) );
       foreach($addInfo->results as $poi) {
@@ -200,11 +298,10 @@
     if(isset($addInfo->next_page_token)) {
       $nextToken = $addInfo->next_page_token;
       unset($GLOBALS['addInfo']);
-      echo '<p><span style="color: green">True Again</span>: Next Token: ' . $nextToken . '</p>';
       callAddToken($nextToken);
 
     } else {
-      echo '<p><span style="color: red">FALSE</span>: No Token!</p>';
+      echo '<p><span style="color: green">Crawled all pages</span>: No Next Page Token left!</p>';
     }
   }
   function formatResult($data) {
