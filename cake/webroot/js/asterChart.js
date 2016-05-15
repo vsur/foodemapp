2ca32f8b8a115e3e980d79835c25e2582e448491
Veit UR <veit.stephan@ukr.de>
2016-05-15 16:11:29 +0200

    Aster Plot First Final
    Aster Plot has now a legend, brighter background arcs a name and is
    suitable.

--- cake/webroot/js/asterChart.js
+++ cake/webroot/js/asterChart.js
@@ -4,6 +4,9 @@
 
 // Setting base for Aster chart
 
+  var legendRectSize = 18;
+  var legendSpacing = 4;
+
 // Get Everything in a function cause it is called recursivly
 
 function drawAster(poiData, chartID) {
@@ -17,7 +20,7 @@ function drawAster(poiData, chartID) {
     width = maxWidth,
     // height = 500 - margin.top - margin.bottom;
     height = maxWidth/2,
-    radius = Math.min(width, height) / 2,
+    radius = Math.min(width, height) / 2 - 15,
     innerRadius = 0.2 * radius;
   var pie = d3.layout.pie()
     .sort(null)
@@ -54,8 +57,6 @@ function drawAster(poiData, chartID) {
   var arc = d3.svg.arc()
     .innerRadius(innerRadius)
     .outerRadius(function (d) {
-      // Evtl nicht durch 100
-      // return (radius - innerRadius) * (d.data._joinData.rating / 100.0) + innerRadius;
       return (radius - innerRadius) * (d.data._joinData.rating * 10 / 100) + innerRadius;
     });
 
@@ -67,31 +68,33 @@ function drawAster(poiData, chartID) {
     .attr("width", width)
     .attr("height", height)
     .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+    .attr("transform", "translate(" + (width / 2 - radius  )+ "," + height / 2 + ")");
 
   var dataset = poiData.components;
+
+  // Background Path
+  var outerPath = svg.selectAll(".outlineArc")
+  .data(pie(dataset))
+  .enter().append("path")
+  // Hellere Farbe hinten einbauen
+  .attr("fill", function(d, i) {
+    return color.range()[i];
+  })
+  .attr("stroke", "gray")
+  .attr("class", "outlineArc")
+  .attr("d", outlineArc);
 
   var path = svg.selectAll(".solidArc")
     .data(pie(dataset))
     .enter().append("path")
-    .attr("fill", function(d, i) { return color(d.data.name + " " + d.data._joinData.rating + "%"); })
-    // .attr("fill", function(d) { return d.data.color; })
+    .attr("fill", function(d) {
+      return d3.rgb(color(d.data.name + " " + d.data._joinData.rating * 10 + "%")).brighter().toString();
+    })
     .attr("class", "solidArc")
     .attr("stroke", "gray")
     .attr("d", arc);
     // .on('mouseover', tip.show)
     // .on('mouseout', tip.hide);
-
-  // HintergrundArc
-  var outerPath = svg.selectAll(".outlineArc")
-    .data(pie(dataset))
-    .enter().append("path")
-    .attr("fill", "none")
-    // Hellere Farbe hinten einbauen
-    // .attr("fill", function(d, i) { return d3.rgb.brighter(color(d.data.name + " " + d.data._joinData.rating + "%")); })
-    .attr("stroke", "gray")
-    .attr("class", "outlineArc")
-    .attr("d", outlineArc);
 
   svg.append("svg:text")
     .attr("class", "aster-score")
@@ -99,9 +102,32 @@ function drawAster(poiData, chartID) {
     .attr("text-anchor", "middle") // text-align: right
     .text(poiData.name);
 
-// TODO LEGENDE einbauen, analog donut
+  var legend = svg.selectAll('.legend')
+    .data(color.domain())
+    .enter()
+    .append('g')
+    .attr('class', 'legend')
+    .attr('transform', function(d, i) {
+      var height = legendRectSize + legendSpacing;
+      // var offset =  height * color.domain().length / 2;
+      var offset =  height * color.domain().length / 2;
+      var horz = -2 * legendRectSize; horz += 200;
+      var vert = i * height - offset;
+      return 'translate(' + horz + ',' + vert + ')';
+    });
 
-// Anschluss dann Karte einbauen!
+  legend.append('rect')
+    .attr('width', legendRectSize)
+    .attr('height', legendRectSize)
+    .style('fill', color)
+    .style('stroke', color);
+
+  legend.append('text')
+    .attr('x', legendRectSize + legendSpacing)
+    .attr('y', legendRectSize - legendSpacing)
+    .text(function(d) { return d; });
+
+  // Anschluss dann Karte einbauen!
 
 }
 
