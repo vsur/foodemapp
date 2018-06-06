4f90dc87b6d1ab4691e4b10643927d2e401bbd55
vsur <v.stephan@evidentmedia.de>
2018-06-06 17:10:55 +0200

    First Try, naja weiter so!

--- cake/src/Controller/Component/D3DataComponent.php
+++ cake/src/Controller/Component/D3DataComponent.php
@@ -5,7 +5,16 @@ namespace App\Controller\Component;
 use Cake\Controller\Component;
 
 class D3DataComponent extends Component {
-    public function buildFilterWheelSuburstJSON($amount1, $amount2) {
-        return $amount1 + $amount2;
+
+    public function buildFilterWheelSuburstJSON($rankedSelection) {
+        $filerWheelData = (object) [{
+            "name" => "filterWheel",
+            "children" => (object) []
+        }];
+
+        
+
+        return $filerWheelJSONData;
     }
+
 }
