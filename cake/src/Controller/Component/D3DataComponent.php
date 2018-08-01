c84b1ec9c7c98ea05c332cd3449b76a815a100db
Veit <v.stephan@evidentmedia.de>
2018-08-01 11:01:23 +0200

    Gecleantes Componentsarray erstellt (ohne Ranked)
    NEXT Index Array bauen.

--- cake/src/Controller/Component/D3DataComponent.php
+++ cake/src/Controller/Component/D3DataComponent.php
@@ -56,6 +56,9 @@ class D3DataComponent extends Component
         $ypoisNames = $this->buildYpoisNamesArray($ypois);
         $allContainedCompponents = $this->getAllComponentNames($ypois);
         debug($ypoisNames);
+        debug($allContainedCompponents);
+        $selectionCleandComponents = $this->removeRankedComponents($allContainedCompponents, $rankedSelection);
+        debug($selectionCleandComponents);
         return $adjacencyMatrix;
     }
 
@@ -158,39 +161,88 @@ class D3DataComponent extends Component
     {
         $allComponentsFromYpois = [];
         foreach ($ypois as $ypoi) {
-
-            foreach ($ypoi->ordinal_attributes as $ordinal_attribute) {
-                // Concat attribute and component and check if display names are present
-                $ordinalComponentAttributeConcatenation =
-                    (empty($ordinal_attribute->ordinal_component->display_name) ? $ordinal_attribute->ordinal_component->name : $ordinal_attribute->ordinal_component->display_name) . ": " .
-                    (empty($ordinal_attribute->display_name) ? $ordinal_attribute->name : $ordinal_attribute->display_name) .
-                    "_OC_C-ID_" . $ordinal_attribute->ordinal_component->id . "_OCATTR-ID_" . $ordinal_attribute->id;
-
-                if(    !in_array($ordinalComponentAttributeConcatenation, $allComponentsFromYpois)   ) {
-                    array_push($allComponentsFromYpois, $ordinalComponentAttributeConcatenation);
-                }
-            }
-
-            foreach ($ypoi->nominal_attributes as $nominal_attribute) {
-                // Concat attribute and component and check if display names are present
-                $nominalComponentAttributeConcatenation =
-                    (empty($nominal_attribute->nominal_component->display_name) ? $nominal_attribute->nominal_component->name : $nominal_attribute->nominal_component->display_name) . ": " .
-                    (empty($nominal_attribute->display_name) ? $nominal_attribute->name : $nominal_attribute->display_name) .
-                    "_NC_C-ID_" . $nominal_attribute->nominal_component->id . "_NCATTR-ID_" . $nominal_attribute->id;
-
-                if(    !in_array($nominalComponentAttributeConcatenation, $allComponentsFromYpois)   ) {
-                    array_push($allComponentsFromYpois, $nominalComponentAttributeConcatenation);
-                }
-            }
-
-            foreach ($ypoi->binary_components as $binary_component) {
-                $binaryComponentConcatenation = (empty($binary_component->display_name) ? $binary_component->name : $binary_component->display_name) . "_BC_C-ID_" . $binary_component->id;
-                if(    !in_array($binaryComponentConcatenation, $allComponentsFromYpois)   ) {
+            foreach ($ypoi->binary_components as $binaryComponent) {
+                // Concat component and id to check if display names are present
+                $binaryComponentConcatenation = $this->buildBinaryComponentConcatenationName($binaryComponent);
+                if (!in_array($binaryComponentConcatenation, $allComponentsFromYpois)) {
                     array_push($allComponentsFromYpois, $binaryComponentConcatenation);
                 }
             }
+            foreach ($ypoi->nominal_attributes as $nominal_attribute) {
+                // Concat attribute and component and ids to check if display names are present
+                $nominalComponentAttributeConcatenation = $this->buildNominalComponentAttributeConcatenationName($nominal_attribute);
+                if (!in_array($nominalComponentAttributeConcatenation, $allComponentsFromYpois)) {
+                    array_push($allComponentsFromYpois, $nominalComponentAttributeConcatenation);
+                }
+            }
+            foreach ($ypoi->ordinal_attributes as $ordinal_attribute) {
+                // Concat attribute and component and check if display names are present
+                $ordinalComponentAttributeConcatenation = $this->buildOrdinalComponentAttributeConcatenationName($ordinal_attribute);
+
+                if (!in_array($ordinalComponentAttributeConcatenation, $allComponentsFromYpois)) {
+                    array_push($allComponentsFromYpois, $ordinalComponentAttributeConcatenation);
+                }
+            }
         }
-        debug($allComponentsFromYpois);
         return $allComponentsFromYpois;
+    }
+
+    protected function removeRankedComponents($allContainedCompponents, $rankedSelection)
+    {
+        foreach ($rankedSelection as $rating => $ratedComponents) {
+            if (!empty($ratedComponents->binaryComponents)) {
+                foreach ($ratedComponents->binaryComponents as $binaryComponent) {
+                    // Concat name for compoarision
+                    $binaryConcatedNameForComparison = $this->buildBinaryComponentConcatenationName($binaryComponent);
+                    if (in_array($binaryConcatedNameForComparison, $allContainedCompponents)) {
+                        $currentIndexToDelete = array_search($binaryConcatedNameForComparison, $allContainedCompponents);
+                        array_splice($allContainedCompponents, $currentIndexToDelete, 1);
+                    }
+                }
+            }
+            if (!empty($ratedComponents->nominalAttributes)) {
+                foreach ($ratedComponents->nominalAttributes as $nominalAttribute) {
+                    // Concat name for compoarision
+                    $nominalConcatedNameForCompoarision = $this->buildNominalComponentAttributeConcatenationName($nominalAttribute);
+                    if (in_array($nominalConcatedNameForCompoarision, $allContainedCompponents)) {
+                        $currentIndexToDelete = array_search($nominalConcatedNameForCompoarision, $allContainedCompponents);
+                        array_splice($allContainedCompponents, $currentIndexToDelete, 1);
+                    }
+                }
+            }
+            if (!empty($ratedComponents->ordinalAttributes)) {
+                foreach ($ratedComponents->ordinalAttributes as $ordinalAttribute) {
+                    // Concat name for compoarision
+                    $ordinalConcatedNameForCompoarision = $this->buildOrdinalComponentAttributeConcatenationName($ordinalAttribute);
+                    if(in_array($ordinalConcatedNameForCompoarision, $allContainedCompponents)) {
+                        $currentIndexToDelete = array_search($ordinalConcatedNameForCompoarision, $allContainedCompponents);
+                        array_splice($allContainedCompponents, $currentIndexToDelete, 1);
+                    }
+                }
+            }
+        }
+        return $allContainedCompponents;
+    }
+
+    protected function buildBinaryComponentConcatenationName($binaryComponent)
+    {
+        $concatedBinaryName = (empty($binaryComponent->display_name) ? $binaryComponent->name : $binaryComponent->display_name) . "_BC_C-ID_" . $binaryComponent->id;
+        return $concatedBinaryName;
+    }
+    protected function buildNominalComponentAttributeConcatenationName($nominalAttribute)
+    {
+        $concatedNominalName =
+            (empty($nominalAttribute->nominal_component->display_name) ? $nominalAttribute->nominal_component->name : $nominalAttribute->nominal_component->display_name) . ": " .
+            (empty($nominalAttribute->display_name) ? $nominalAttribute->name : $nominalAttribute->display_name) .
+            "_NC_C-ID_" . $nominalAttribute->nominal_component->id . "_NCATTR-ID_" . $nominalAttribute->id;
+        return $concatedNominalName;
+    }
+    protected function buildOrdinalComponentAttributeConcatenationName($ordinalAttribute)
+    {
+        $concatedOrdinalName =
+            (empty($ordinalAttribute->ordinal_component->display_name) ? $ordinalAttribute->ordinal_component->name : $ordinalAttribute->ordinal_component->display_name) . ": " .
+            (empty($ordinalAttribute->display_name) ? $ordinalAttribute->name : $ordinalAttribute->display_name) .
+            "_OC_C-ID_" . $ordinalAttribute->ordinal_component->id . "_OCATTR-ID_" . $ordinalAttribute->id;
+        return $concatedOrdinalName;
     }
 }
