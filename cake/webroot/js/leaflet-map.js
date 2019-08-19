4aad2ddf553194d45f4786415d24ba07f6ba3ef7
Veit <v.stephan@oth-aw.de>
2019-08-19 12:33:33 +0200

    Eigen Icons hinzugefügt. Geht soweit auch … nur dass Adden des Textes ist schwierig. Besser den DivIcon nehmen. Also noch mal umbauen.

--- cake/webroot/js/leaflet-map.js
+++ cake/webroot/js/leaflet-map.js
@@ -3,11 +3,22 @@
 // First set height for LMap
 var maxHeight = window.innerHeight;
 $("#ypoisMap").css("height", maxHeight/2);
+console.log(window.location);
+
 
 // Initalize LMap
 var mymap = L.map('ypoisMap').setView([49.01, 8.40806], 13);
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
+var myIcon = L.icon({
+    // iconUrl: 'foodmap/cake/img/leaflet/marker-food.png',
+    iconUrl: '../../img/leaflet/marker-isac.png',
+    iconSize: [32, 32],
+    iconAnchor: [16, 16],
+    popupAnchor: [-3, -76],
+    shadowUrl: '../../img/leaflet/marker-food-isac-shadow.png',
+    shadowSize: [32, 32],
+    shadowAnchor: [16, 16]
+});
 L.tileLayer.provider('OpenStreetMap.BlackAndWhite').addTo(mymap);
 ypois.forEach(function(ypoi, i) {
-    var marker = L.marker([ypoi.lat, ypoi.lng]).addTo(mymap);
+    var marker = L.marker([ypoi.lat, ypoi.lng], {icon: myIcon}).addTo(mymap);
 });
