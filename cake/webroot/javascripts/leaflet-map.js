951f9413acb471b50f646cae68be3631212fdc6b
Veit <v.stephan@oth-aw.de>
2019-08-19 16:29:48 +0200

    Icon und Label Save Next ist dann das PopUp.

--- cake/webroot/javascripts/leaflet-map.js
+++ cake/webroot/javascripts/leaflet-map.js
@@ -3,22 +3,18 @@
 // First set height for LMap
 var maxHeight = window.innerHeight;
 $("#ypoisMap").css("height", maxHeight/2);
-console.log(window.location);
-
 
 // Initalize LMap
 var mymap = L.map('ypoisMap').setView([49.01, 8.40806], 13);
-var myIcon = L.icon({
-    // iconUrl: 'foodmap/cake/img/leaflet/marker-food.png',
-    iconUrl: '../../img/leaflet/marker-isac.png',
+var myIcon = L.divIcon({
     iconSize: [32, 32],
     iconAnchor: [16, 16],
     popupAnchor: [-3, -76],
-    shadowUrl: '../../img/leaflet/marker-food-isac-shadow.png',
-    shadowSize: [32, 32],
-    shadowAnchor: [16, 16]
 });
 L.tileLayer.provider('OpenStreetMap.BlackAndWhite').addTo(mymap);
 ypois.forEach(function(ypoi, i) {
-    var marker = L.marker([ypoi.lat, ypoi.lng], {icon: myIcon}).addTo(mymap);
+    var newIcon = myIcon;
+    newIcon.options.className = 'ypoiIcon';
+    newIcon.options.html = '<div><span>' + ypoi.name + '</span></div>';
+    var marker = L.marker([ypoi.lat, ypoi.lng], {icon: newIcon}).addTo(mymap);
 });
