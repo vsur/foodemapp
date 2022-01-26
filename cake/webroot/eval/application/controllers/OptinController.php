338987597c876a92c816cbd7d8ce4e75140fb677
Veit Stephan EM <veit.stephan@ukr.de>
2022-01-26 17:12:00 +0100

    Lime Survey als Sub Dir wg. Cors

---
+++ cake/webroot/eval/application/controllers/OptinController.php
@@ -0,0 +1,105 @@
+<?php
+
+/*
+ * LimeSurvey
+ * Copyright (C) 2013 The LimeSurvey Project Team / Carsten Schmitz
+ * All rights reserved.
+ * License: GNU/GPL License v2 or later, see LICENSE.php
+ * LimeSurvey is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ *
+ *
+ *
+ */
+
+/**
+ * optin
+ *
+ * @package LimeSurvey
+ * @copyright 2011
+ * @access public
+ */
+class OptinController extends LSYii_Controller
+{
+
+    public $layout = 'bare';
+    public $defaultAction = 'tokens';
+
+    function actiontokens($surveyid, $token, $langcode = '')
+    {
+        Yii::app()->loadHelper('database');
+        Yii::app()->loadHelper('sanitize');
+        $sLanguageCode = $langcode;
+        $iSurveyID = $surveyid;
+        $oSurvey = Survey::model()->findByPk($iSurveyID);
+        $sToken = $token;
+        $sToken = Token::sanitizeToken($sToken);
+
+        if (!$iSurveyID) {
+            $this->redirect(array('/'));
+        }
+        $iSurveyID = $oSurvey->primaryKey;
+
+        //Check that there is a SID
+        // Get passed language from form, so that we dont loose this!
+        if (!isset($sLanguageCode) || $sLanguageCode == "" || !$sLanguageCode) {
+            $sBaseLanguage = $oSurvey->language;
+        } else {
+            $sBaseLanguage = sanitize_languagecode($sLanguageCode);
+        }
+
+        Yii::app()->setLanguage($sBaseLanguage);
+
+        if (empty($oSurvey) || !$oSurvey->hasTokensTable) {
+            throw new CHttpException(404, "This survey does not seem to exist. It may have been deleted or the link you were given is outdated or incorrect.");
+        } else {
+            LimeExpressionManager::singleton()->loadTokenInformation($iSurveyID, $sToken, false);
+            $oToken = Token::model($iSurveyID)->findByAttributes(array('token' => $sToken));
+
+            if (!isset($oToken)) {
+                $sMessage = gT('You are not a participant of this survey.');
+            } else {
+                if ($oToken->emailstatus == 'OptOut') {
+                    $oToken->emailstatus = 'OK';
+                    $oToken->save();
+                    $sMessage = gT('You have been successfully added back to this survey.');
+                } elseif ($oToken->emailstatus == 'OK') {
+                    $sMessage = gT('You are already a participant of this survey.');
+                } else {
+                    $sMessage = gT('You have been already removed from this survey.');
+                }
+            }
+        }
+
+        $this->renderHtml($sMessage, $oSurvey);
+    }
+
+    /**
+     * Render stuff
+     *
+     * @param string $html
+     * @param Survey $survey
+     * @return void
+     */
+    private function renderHtml($html, $survey)
+    {
+        $aSurveyInfo = getSurveyInfo($survey->primaryKey);
+
+        $aSurveyInfo['include_content'] = 'optin';
+        $aSurveyInfo['optin_message'] = $html;
+        Template::getInstance('', $survey->primaryKey);
+
+        Yii::app()->twigRenderer->renderTemplateFromFile(
+            "layout_global.twig",
+            array(
+                'oSurvey'     => $survey,
+                'aSurveyInfo' => $aSurveyInfo
+            ),
+            false
+        );
+        Yii::app()->end();
+    }
+}
---
+++ cake/webroot/eval/application/models/FailedLoginAttempt.php
@@ -0,0 +1,184 @@
+<?php
+
+/*
+ * LimeSurvey
+ * Copyright (C) 2007-2011 The LimeSurvey Project Team / Carsten Schmitz
+ * All rights reserved.
+ * License: GNU/GPL License v2 or later, see LICENSE.php
+ * LimeSurvey is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ *
+ */
+
+/**
+ * Class FailedLoginAttempt
+ *
+ * @property integer $id
+ * @property string $ip Ip address
+ * @property string $last_attempt
+ * @property integer $number_attempts
+ */
+class FailedLoginAttempt extends LSActiveRecord
+{
+    const TYPE_LOGIN = 'login';
+    const TYPE_TOKEN = 'token';
+
+    /**
+     * @inheritdoc
+     * @return FailedLoginAttempt
+     */
+    public static function model($class = __CLASS__)
+    {
+        /** @var self $model */
+        $model = parent::model($class);
+        return $model;
+    }
+
+    /** @inheritdoc */
+    public function primaryKey()
+    {
+        return 'id';
+    }
+
+    /** @inheritdoc */
+    public function tableName()
+    {
+        return '{{failed_login_attempts}}';
+    }
+
+    /**
+     * Deletes all the attempts by IP
+     *
+     * @access public
+     * @return void
+     */
+    public function deleteAttempts()
+    {
+        $ip = substr(getIPAddress(), 0, 40);
+        $this->deleteAllByAttributes(array('ip' => $ip));
+    }
+
+    /**
+     * Check if an IP address is allowed to login or not
+     *
+     * @param string $attemptType  The attempt type ('login' or 'token'). Used to check the white lists.
+     *
+     * @return boolean Returns true if the user is blocked
+     */
+    public function isLockedOut($attemptType = '')
+    {
+        $isLockedOut = false;
+        $ip = substr(getIPAddress(), 0, 40);
+
+        // Return false if IP is whitelisted
+        if (!empty($attemptType) && $this->isWhitelisted($ip, $attemptType)) {
+            return false;
+        }
+
+        $criteria = new CDbCriteria();
+        $criteria->condition = 'number_attempts > :attempts AND ip = :ip';
+        $criteria->params = array(':attempts' => Yii::app()->getConfig('maxLoginAttempt'), ':ip' => $ip);
+
+        $row = $this->find($criteria);
+
+        if ($row != null) {
+            $lastattempt = strtotime($row->last_attempt);
+            if (time() > $lastattempt + Yii::app()->getConfig('timeOutTime')) {
+                $this->deleteAttempts();
+            } else {
+                $isLockedOut = true;
+            }
+        }
+        return $isLockedOut;
+    }
+
+    /**
+     * This function removes obsolete login attempts
+     * TODO
+     */
+    public function cleanOutOldAttempts()
+    {
+        // this where select whole part
+        //$this->db->where('now() > (last_attempt+'.$this->config->item("timeOutTime").')');
+        //return $this->db->delete('failed_login_attempts');
+    }
+
+    /**
+     * Records an failed login-attempt if IP is not already locked out
+     *
+     * @access public
+     * @return true
+     */
+    public function addAttempt()
+    {
+        if (!$this->isLockedOut()) {
+            $timestamp = date("Y-m-d H:i:s");
+            $ip = substr(getIPAddress(), 0, 40);
+            $row = $this->findByAttributes(array('ip' => $ip));
+
+            if ($row !== null) {
+                $row->number_attempts = $row->number_attempts + 1;
+                $row->last_attempt = $timestamp;
+                $row->save();
+            } else {
+                $record = new FailedLoginAttempt();
+                $record->ip = $ip;
+                $record->number_attempts = 1;
+                $record->last_attempt = $timestamp;
+                $record->save();
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Returns true if the specified IP is whitelisted
+     *
+     * @param string $ip
+     * @param string $attemptType   'login' or 'token'
+     *
+     * @throws InvalidArgumentException if an invalid attempt type is specified
+     * @return boolean
+     */
+    private function isWhitelisted($ip, $attemptType)
+    {
+        // Init
+        if ($attemptType != self::TYPE_LOGIN && $attemptType != self::TYPE_TOKEN) {
+            throw new InvalidArgumentException(sprintf("Invalid attempt type: %s", $attemptType));
+        }
+        if (empty($ip)) {
+            return false;
+        }
+        $binaryIP = inet_pton($ip);
+
+        $whiteList = Yii::app()->getConfig($attemptType . 'IpWhitelist');
+        if (empty($whiteList)) {
+            return false;
+        }
+
+        // Validating
+        $whiteListEntries = preg_split('/\n|,/', $whiteList);
+        foreach ($whiteListEntries as $whiteListEntry) {
+            if (empty($whiteListEntry)) {
+                continue;
+            }
+            // Compare directly
+            if ($whiteListEntry == $ip) {
+                // The IP is whitelisted
+                return true;
+            }
+            // Compare binary representations
+            $binaryWhiteListEntry = inet_pton($whiteListEntry);
+            if ($binaryWhiteListEntry !== false && $binaryWhiteListEntry == $binaryIP) {
+                // The IP is whitelisted
+                return true;
+            }
+        }
+
+        // Not whitelisted
+        return false;
+    }
+}
