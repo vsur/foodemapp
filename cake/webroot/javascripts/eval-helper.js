c71a7e30be901fd6384ad9d08609d056d4367812
Veit Stephan <veit.stephan@ukr.de>
2022-01-28 15:50:39 +0100

    Alle Ids für Antworten zum Pasten der Quantity Data eingefügt

--- cake/webroot/javascripts/eval-helper.js
+++ cake/webroot/javascripts/eval-helper.js
@@ -38,10 +38,40 @@ function saveQuantityData(event, view) {
         case "list":
             // mMove Data Textarea ID: answer612158X5X93
             $("#answer612158X5X93").val(JSON.stringify(mouseData.mMove.data));
-            // mClicl Data Textarea ID: answer612158X5X94
+            // mClick Data Textarea ID: answer612158X5X94
             $("#answer612158X5X94").val(JSON.stringify(mouseData.mClick.data));
             // aoi.list.pois Data Textarea ID: answer612158X5X93
             $("#answer612158X5X95").val(JSON.stringify(mouseData.aoi.list.pois));
+            break;
+
+        case "chord":
+            // mMove Data Textarea ID: answer612158X16X96
+            $("#answer612158X16X96").val(JSON.stringify(mouseData.mMove.data));
+            // mClick Data Textarea ID: answer612158X16X97
+            $("#answer612158X16X97").val(JSON.stringify(mouseData.mClick.data));
+            // aoi.chord.pois Data Textarea ID: answer612158X16X98
+            $("#answer612158X16X98").val(JSON.stringify(mouseData.aoi.chord.pois));
+            // aoi.chord.choosenComponents Data Textarea ID: answer612158X16X99
+            $("#answer612158X16X99").val(JSON.stringify(mouseData.aoi.chord.choosenComponents));
+            // aoi.chord.otherComponents Data Data Textarea ID: answer612158X16X100
+            $("#answer612158X16X100").val(JSON.stringify(mouseData.aoi.chord.otherComponents));
+            break;
+
+        case "map":
+            // mMove Data Textarea ID: answer612158X17X101
+            $("#answer612158X17X101").val(JSON.stringify(mouseData.mMove.data));
+            // mClick Data Textarea ID: answer612158X17X102
+            $("#answer612158X17X102").val(JSON.stringify(mouseData.mClick.data));
+            // aoi.map.mapComponentsChoice Data Textarea ID: answer612158X17X103
+            $("#answer612158X17X103").val(JSON.stringify(mouseData.aoi.map.mapComponentsChoice));
+            // aoi.map.pois Data Textarea ID: answer612158X17X104
+            $("#answer612158X17X104").val(JSON.stringify(mouseData.aoi.map.pois));
+            // aoi.map.dragMap Data Textarea ID: answer612158X17X105
+            $("#answer612158X17X105").val(JSON.stringify(mouseData.aoi.map.dragMap));
+            // aoi.map.dragPopup Data Textarea ID: answer612158X17X106
+            $("#answer612158X17X106").val(JSON.stringify(mouseData.aoi.map.dragPopup));
+            // aoi.map.zoomMap Data Textarea ID: answer612158X17X107
+            $("#answer612158X17X107").val(JSON.stringify(mouseData.aoi.map.zoomMap));
             break;
 
         default:
