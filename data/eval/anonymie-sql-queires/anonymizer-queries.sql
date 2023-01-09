bdebd0bef1761c06b8d47816b538e0d8e20b3b8b
Veit Stephan EM <veit.stephan@ukr.de>
2023-01-09 14:36:16 +0100

    Zusätzliche Anonymizer Queries.

---
+++ data/eval/anonymie-sql-queires/anonymizer-queries.sql
@@ -0,0 +1,5 @@
+# Abschlussanmerkung anonymisieren
+UPDATE `participants` SET `612158X8X54` = "ananymized data" WHERE `612158X8X54` LIKE '%@%'
+
+# Datenschutz Löschfeld anonymisieren
+UPDATE `participants` SET `612158X9X73` = "ananymized data" WHERE `612158X9X73` LIKE '%@%'
